b237c1604bef46b7ccada10bb657aea6e1b52237
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2025-11-25 15:37:52 -0500

    add analytical Jacobian and improve color scales

--- code/scripts/fig4.R
+++ code/scripts/fig4.R
@@ -9,7 +9,7 @@ source("code/functions/hastings_powell_3sp_ode.R")
 # to reproduce results 
 set.seed(42)
 # whether to save plots
-save_plots <- FALSE
+save_plots <- TRUE
 # model to use
 func_name <- "rosenzweig_macarthur_2sp_forced_fixed_point"
 # load model settings
@@ -20,9 +20,15 @@ sp_names <- paste("x", 1:n_sp, sep = "")
 sample_size <- 50 * 2^n_sp
 # color palettes
 palette <- colorRampPalette(brewer.pal(11, "RdYlBu"))(sample_size)
-# percentage of recurrence time to evolve perturbations
+# percentage of recurrence time to evolve perturbations (set to 0.2 or 0.8 to produce other plots in Fig 4)
 perc_rec_time <- 0.02
 tau <- time_step * n_points * perc_rec_time
+# which stability metric to use 
+# expect_avg_growth_rate for Fig 4 
+# max_growth_rate for Fig S9 
+# max_growth_rate_eigen for Fig S10
+# expect_inst_growth_rate for Fig S11
+metric <- "expect_avg_growth_rate"
 
 # generate non-perturbed abundance trajectory ------------------------------
 # generate time series
@@ -47,10 +53,8 @@ if (func_name == "hastings_powell_3sp_chaos") {
 }
 
 # compute Jacobian matrix along trajectory ------------------------------
-J <- dlply(ts, "time", function(x) jacobian.full(y = unlist(c(x[2:(n_sp + 1)])), 
-                                                 func = func,
-                                                 parms = parms))
-if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
+if ((func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") | 
+    (func_name == "rosenzweig_macarthur_2sp_limit_cycle")) {
   J <- list()
   x1 <- ts$x1
   x2 <- ts$x2
@@ -59,6 +63,24 @@ if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
                        - (a * x1[i]) / (b + x1[i]),
                        (e * a * x2[i] * (b + x1[i]) - e * a * x1[i] * x2[i]) / (b + x1[i])^2, 
                        ((e * a * x1[i]) / (b + x1[i])) - d), nrow = 2, byrow = TRUE)
+  }
+}
+if ((func_name == "hastings_powell_3sp_forced_cycle") |
+    (func_name == "hastings_powell_3sp_chaos")) {
+  J <- list()
+  x1 <- ts$x1
+  x2 <- ts$x2
+  x3 <- ts$x3
+  for (i in 1:nrow(ts)) {
+    J[[i]] <- matrix(c((1 - (x1[i] / k)) - x1[i] * (1 / k) - (a1 * x2[i] / (x1[i] + b1) - (a1 * x1[i] * x2[i]) / (x1[i] + b1)^2),
+                       -(a1 * x1[i] / (x1[i] + b1)),
+                       0,
+                       a1 * e1 * x2[i] / (x1[i] + b1) - (a1 * e1 * x1[i] * x2[i]) / (x1[i] + b1)^2,
+                       a1 * e1 * x1[i] / (x1[i] + b1) - d1 - (a2[i] * x3[i] / (x2[i] + b2) - (a2[i] * x2[i] * x3[i]) / (x2[i] + b2)^2),
+                       -(a2[i] * x2[i] / (x2[i] + b2)),
+                       0,
+                       a2[i] * e2 * x3[i] / (x2[i] + b2) - (a2[i] * e2 * x2[i] * x3[i]) / (x2[i] + b2)^2,
+                       a2[i] * e2 * x2[i] / (x2[i] + b2) - d2), nrow = 3, byrow = TRUE)
   }
 }
 names(J) <- sprintf("%.5f", ts$time)
@@ -90,29 +112,86 @@ for (j in 1:nrow(ts_sub)) {
   expect_inst_growth_rate[j] <- log(det(Phi)) / (n_sp * tau)
 }
 plot_df <- ts_sub
-plot_df$expect_avg_growth_rate <- expect_avg_growth_rate
-min_axis <- min(plot_df$expect_avg_growth_rate)
-max_axis <- max(plot_df$expect_avg_growth_rate)
-if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
-  axis_edge <- 0.41
-  legend_breaks <- c(-0.4, -0.2, 0, 0.2, 0.4)
+if (metric == "expect_avg_growth_rate") {
+  plot_df$metric <- expect_avg_growth_rate
+  if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
+    axis_edge <- 0.405
+    legend_breaks <- c(-0.4, -0.2, 0, 0.2, 0.4)
+  }
+  if (func_name == "rosenzweig_macarthur_2sp_limit_cycle") {
+    axis_edge <- 1.02
+    legend_breaks <- c(-1, -0.5, 0, 0.5, 1)
+  }
+  if (func_name == "hastings_powell_3sp_forced_cycle") {
+    axis_edge <- 0.065
+    legend_breaks <- c(-0.06, -0.03, 0, 0.03, 0.06)
+  }
+  if (func_name == "hastings_powell_3sp_chaos") {
+    axis_edge <- 0.13
+    legend_breaks <- c(-0.12, -0.06, 0, 0.06, 0.12)
+  }
 }
-if (func_name == "rosenzweig_macarthur_2sp_limit_cycle") {
-  axis_edge <- 1.02
-  legend_breaks <- c(-1, -0.5, 0, 0.5, 1)
+if (metric == "max_growth_rate") {
+  plot_df$metric <- max_growth_rate
+  if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
+    axis_edge <- 1.07
+    legend_breaks <- c(-1, -0.5, 0, 0.5, 1)
+  }
+  if (func_name == "rosenzweig_macarthur_2sp_limit_cycle") {
+    axis_edge <- 1.71
+    legend_breaks <- c(-1.5, -0.75, 0, 0.75, 1.5)
+  }
+  if (func_name == "hastings_powell_3sp_forced_cycle") {
+    axis_edge <- 0.255
+    legend_breaks <- c(-0.2, -0.1, 0, 0.1, 0.2)
+  }
+  if (func_name == "hastings_powell_3sp_chaos") {
+    axis_edge <- 0.4
+    legend_breaks <- c(-0.4, -0.2, 0, 0.2, 0.4)
+  }
 }
-if (func_name == "hastings_powell_3sp_forced_cycle") {
-  axis_edge <- 0.08
-  legend_breaks <- c(-0.08, -0.04, 0, 0.04, 0.08)
+if (metric == "max_growth_rate_eigen") {
+  plot_df$metric <- max_growth_rate_eigen
+  if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
+    axis_edge <- 1.07
+    legend_breaks <- c(-1, -0.5, 0, 0.5, 1)
+  }
+  if (func_name == "rosenzweig_macarthur_2sp_limit_cycle") {
+    axis_edge <- 1.71
+    legend_breaks <- c(-1.5, -0.75, 0, 0.75, 1.5)
+  }
+  if (func_name == "hastings_powell_3sp_forced_cycle") {
+    axis_edge <- 0.255
+    legend_breaks <- c(-0.2, -0.1, 0, 0.1, 0.2)
+  }
+  if (func_name == "hastings_powell_3sp_chaos") {
+    axis_edge <- 0.4
+    legend_breaks <- c(-0.4, -0.2, 0, 0.2, 0.4)
+  }
 }
-if (func_name == "hastings_powell_3sp_chaos") {
-  axis_edge <- 0.13
-  legend_breaks <- c(-0.12, -0.06, 0, 0.06, 0.12)
+if (metric == "expect_inst_growth_rate") {
+  plot_df$metric <- expect_inst_growth_rate
+  if (func_name == "rosenzweig_macarthur_2sp_forced_fixed_point") {
+    axis_edge <- 0.51
+    legend_breaks <- c(-0.5, -0.25, 0, 0.25, 0.5)
+  }
+  if (func_name == "rosenzweig_macarthur_2sp_limit_cycle") {
+    axis_edge <- 1.02
+    legend_breaks <- c(-1, -0.5, 0, 0.5, 1)
+  }
+  if (func_name == "hastings_powell_3sp_forced_cycle") {
+    axis_edge <- 0.21
+    legend_breaks <- c(-0.2, -0.1, 0, 0.1, 0.2)
+  }
+  if (func_name == "hastings_powell_3sp_chaos") {
+    axis_edge <- 0.27
+    legend_breaks <- c(-0.2, -0.1, 0, 0.1, 0.2)
+  }
 }
 # plot abundance trajectory colored by stability metric (for 2 species)
 if (n_sp == 2) {
   fig <- ggplot() +
-    geom_point(data = plot_df, aes(x = x1, y = x2, color = expect_avg_growth_rate), 
+    geom_point(data = plot_df, aes(x = x1, y = x2, color = metric), 
                size = 5) +
     geom_path(data = ts_example, aes(x = x1, y = x2), size = 1, color = "black", 
               arrow = arrow(ends = "last", length = unit(0.2, "inches"))) +
@@ -137,7 +216,7 @@ if (n_sp == 2) {
   # plot to extract color scale
   if (perc_rec_time == 0.02) {
     fig <- ggplot() +
-      geom_point(data = plot_df, aes(x = x1, y = x2, color = expect_avg_growth_rate), 
+      geom_point(data = plot_df, aes(x = x1, y = x2, color = metric), 
                  size = 5) +
       geom_path(data = ts_example, aes(x = x1, y = x2), size = 1, color = "black", 
                 arrow = arrow(ends = "last", length = unit(0.2, "inches"))) +
@@ -169,7 +248,7 @@ if (n_sp == 2) {
 if (n_sp == 3) {
   fig <- plot_ly(x = ~x1, y = ~x2, z = ~x3, colors = palette, showlegend = FALSE) %>% 
     add_markers(data = plot_df, type = 'scatter3d', marker = list(size = 8),
-                color = ~expect_avg_growth_rate) %>% 
+                color = ~metric) %>% 
     add_trace(data = ts_example, type = 'scatter3d', mode = "lines",
               color = I('black'), line = list(width = 8)) %>% 
     colorbar(limits = c(-axis_edge, axis_edge), title = "Average\nperturbation\ngrowth\nrate", orientation = "v") %>% 
@@ -220,7 +299,7 @@ if (n_sp == 3) {
   # plot to extract color scale
   if (perc_rec_time == 0.02) { 
     fig <- ggplot() +
-      geom_point(data = plot_df, aes(x = x1, y = x2, color = expect_avg_growth_rate), 
+      geom_point(data = plot_df, aes(x = x1, y = x2, color = metric), 
                  size = 5) +
       scale_color_gradientn(colors = brewer.pal(11, "RdYlBu"), 
                             limits = c(-axis_edge, axis_edge), breaks = legend_breaks) +
